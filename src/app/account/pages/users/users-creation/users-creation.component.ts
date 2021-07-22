703ef849b2fd10a1f477add7130df4020a723ea5
Michelle Alleyne <michelle.alleyne@newtoms.com>
2021-07-22 00:03:06 -0400

    Error messages added

--- src/app/account/pages/users/users-creation/users-creation.component.ts
+++ src/app/account/pages/users/users-creation/users-creation.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
-import { emailPattern } from 'src/app/core/config/configuration';
+import { TranslateService } from '@ngx-translate/core';
+import { address, emailPattern } from 'src/app/core/config/configuration';
 import { UsersService } from 'src/app/core/services/users.service';
 import { NotificationsService } from 'src/app/shared/services/notifications.service';
+import { searchTranslation } from 'src/app/utils/searchTranslation';
 
 @Component({
   selector: 'app-users-creation',
@@ -13,37 +15,53 @@ import { NotificationsService } from 'src/app/shared/services/notifications.serv
 export class UsersCreationComponent implements OnInit {
   userForm!: FormGroup;
   spinnerLoader = false;
-  cities = ['Ciudad de México']
   userTypes = [ 'agent', 'admin']
-  countries = [ 'MEXICO', 'UNITED_STATES']
+  addresses = address;
+
 
   constructor(   
     private formBuilder: FormBuilder,
     private notification: NotificationsService,
     public dialogRef: MatDialogRef<UsersCreationComponent>,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private translateService: TranslateService
   ) { }
 
   ngOnInit(): void {
     this.buildForm();
   }
 
+
+  formatContent(value: string): string {
+    return ((searchTranslation(this.translateService, value.toUpperCase())).replace('_', ' '))
+  }
+
+
+  public searchStates(
+    selectedCountry: string
+  ): String [] | [] {
+    const country = this.addresses.find((country) => country.country === selectedCountry);
+
+    return country ?  country.cities : []
+  }
+
   public createUser(): void {
     this.userForm.markAllAsTouched();
 
     if (this.userForm.valid) {
-      console.log(this.userForm)
       this.spinnerLoader = true;
+      const {country: selectedCountry, ...userData} = this.userForm.value
 
-      const values = this.userForm.value
-      this.usersService.create(values).toPromise()
+      const user = { ...userData, country:  this.addresses.find((country) => country.country === selectedCountry)?.code }
+
+      this.usersService.create(user).toPromise()
       .then(() => {
-        this.notification.showSuccessToast('USER_REGISTERED')
+        this.notification.showSuccessToast('USER_CREATED')
       })
       .catch((error) => {
         this.spinnerLoader = false;
-        console.log(error)
-        this.notification.showErrorToast(error);
+        const errorDetail = error.includes("already exists") ? 'USER_ALREADY_EXISTS' : 'GENERIC_ERROR'
+        this.notification.showErrorToast(errorDetail);
       });
   
     }
@@ -56,8 +74,8 @@ export class UsersCreationComponent implements OnInit {
       last_name: ['', Validators.compose([Validators.required])],
       phone_number: ['', Validators.compose([Validators.required])],
       role: ['agent', Validators.compose([Validators.required])],
-      country: ['UNITED_STATES',  Validators.compose([Validators.required])],
-      city:  ['Colorado', Validators.compose([Validators.required])],
+      country: ['México',  Validators.compose([Validators.required])],
+      city:  ['Aguascalientes', Validators.compose([Validators.required])],
       detail_address:  [''],
     });
   }
