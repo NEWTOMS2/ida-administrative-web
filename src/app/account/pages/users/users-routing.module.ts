cf3af9818641f7d909a6567560c98230a035de5b
Michelle Alleyne <77360967+alleynemichelle@users.noreply.github.com>
2021-07-22 20:59:54 -0400

    AW-005: Como administrador del OMV, quiero registrar a usuarios agentes para que puedan acceder al sistema con sus credenciales. (#8)
    * Registration Form
    
    * Error messages added
    
    * User registration added

--- src/app/account/pages/users/users-routing.module.ts
+++ src/app/account/pages/users/users-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserResolver } from '../../resolvers/user.resolver';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent,
+    resolve: {
+      user: UserResolver,
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ src/app/core/services/users.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError, from } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+
+import { administrative_exp_api_host } from '../config/configuration';
+import { User } from '../models/user.interface';
+import { AuthService } from './auth.service';
+import { environment } from 'src/environments/environment';
+
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  token = this.authService.decrypt(localStorage.getItem('Token') || "", environment.encryptKey)
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.token}`
+
+    })
+  }  
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) {}
+
+  create(user: User): Observable<any> {
+      console.log(user)
+    return this.http.post(administrative_exp_api_host + '/users', [user], this.httpOptions)
+    .pipe(
+      catchError(this.handleError)
+    )
+  }
+
+
+  handleError(error: any ) {
+    console.log(error);
+    return throwError(error.error.error[0].error_description);
+ }
+
+}
