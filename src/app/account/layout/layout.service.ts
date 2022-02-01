8681e867c656793cb45db9851536a50781fdcc16
diegodequintal <dadequintal.17@est.ucab.edu.ve>
2022-02-01 12:01:19 -0400

    Logout connect session and create client field fixed

--- src/app/account/layout/layout.service.ts
+++ src/app/account/layout/layout.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class LayoutService {
   sidenav: any;
   drawerContainer: any;
+
+  private logOutSubject = new Subject<boolean>();
+  logOutObservable = this.logOutSubject.asObservable();
+
+  closeAwsConnectSession(): void {
+    this.logOutSubject.next(true);
+  }
 }
